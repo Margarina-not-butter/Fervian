c32ef3ac19bfdc41b5965f54039f369f1392168f
Adrian Victor <adrianvictor@disroot.org>
2025-05-15 11:02:48 -0300

    Fixed sunken table.

--- res/fervian.js
+++ res/fervian.js
@@ -28,3 +28,18 @@ function showPopup(title, message, callback, classes) {
         document.body.removeChild(overlay);
     });
 }
+
+document.querySelectorAll('table.interactive').forEach(element => {
+    element.addEventListener('click', (event) => {
+        const highlightedClass = 'sunken-highlighted';
+        const isRow = element => element.tagName === 'TR' && element.parentElement.tagName === 'TBODY';
+        const newlySelectedRow = event.composedPath().find(isRow);
+        const previouslySelectedRow = Array.from(newlySelectedRow.parentElement.children).filter(isRow).find(element => element.classList.contains(highlightedClass));
+        if (previouslySelectedRow && previouslySelectedRow !== newlySelectedRow) {
+            previouslySelectedRow.classList.remove(highlightedClass);
+        }
+        if (newlySelectedRow) {
+            newlySelectedRow.classList.add(highlightedClass);
+        }
+    });
+});
