ba903794021de7d6c93d33d8e86239c16b627a92
Adrian Victor <adrianvictor@disroot.org>
2025-05-15 10:50:52 -0300

    Language selection screen, portuguese translation.

--- res/index.js
+++ res/index.js
@@ -1,22 +1,7 @@
 const loadBlogPost = document.getElementById("loadPostButton");
 
-document.querySelectorAll('table.interactive').forEach(element => {
-    element.addEventListener('click', (event) => {
-        const highlightedClass = 'blog-highlighted';
-        const isRow = element => element.tagName === 'TR' && element.parentElement.tagName === 'TBODY';
-        const newlySelectedRow = event.composedPath().find(isRow);
-        const previouslySelectedRow = Array.from(newlySelectedRow.parentElement.children).filter(isRow).find(element => element.classList.contains(highlightedClass));
-        if (previouslySelectedRow && previouslySelectedRow !== newlySelectedRow) {
-            previouslySelectedRow.classList.remove(highlightedClass);
-        }
-        if (newlySelectedRow) {
-            newlySelectedRow.classList.add(highlightedClass);
-        }
-    });
-});
-
 loadBlogPost.addEventListener('click', () => {
-    const selected = document.querySelector('.blog-highlighted');
+    const selected = document.querySelector('.sunken-highlighted');
     const post = selected.dataset.post;
     window.location.href = `blogpost.html?id=${post}`
 })
